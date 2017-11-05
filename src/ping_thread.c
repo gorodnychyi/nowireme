1ea3cee2ab9672ade5aa279d0f37545b86af4aed
gall <gall.ghn@gmail.com>
2017-11-05 18:15:51 +0100

    4

--- src/ping_thread.c
+++ src/ping_thread.c
@@ -197,7 +197,7 @@ ping(void)
         }
         free(res);
     } else if ((strstr(res, "Update") != 0) ||  (strstr(res, "Pong") !=0)) {
-        debug(LOG_DEBUG, "Server says: Pong/Update %s", get_gw_mac());
+        debug(LOG_DEBUG, "Server says: Pong/Update %s", get_gw_mac(mac_address));
         if (authdown) {
             fw_set_authup();
             authdown = 0;
